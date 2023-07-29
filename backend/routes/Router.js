9093fb5a2e1046ef0071e37f6db6f9cb1083ea1f
Igor-Maceno <igorlopesmaceno17@gmail.com>
2023-07-28 21:30:42 -0300

    User controller
    Criação dos controllers de usuário.

--- backend/routes/Router.js
+++ backend/routes/Router.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express();
 
+router.use("/api/user", require("./UserRoutes"));
+
 //test route
 router.get("/", (req, res) => {
     res.send("API WORKING!");
